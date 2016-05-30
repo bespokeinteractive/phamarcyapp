9662ac38e9df172dde1eec31825fe21ee030ef91
stansL <mstans8@gmail.com>
2016-05-30 15:18:06 +0300

    waiver amount fix

--- omod/src/main/java/org/openmrs/module/pharmacyapp/page/controller/PrintDrugOrderPageController.java
+++ omod/src/main/java/org/openmrs/module/pharmacyapp/page/controller/PrintDrugOrderPageController.java
@@ -147,7 +147,7 @@ public class PrintDrugOrderPageController {
             model.addAttribute("waiverAmount", listDrugIssue.get(0).getStoreDrugPatient().getWaiverAmount());
             model.addAttribute("waiverComment", listDrugIssue.get(0).getStoreDrugPatient().getComment());
         }else{
-            model.addAttribute("waiverAmount", 0);
+            model.addAttribute("waiverAmount", "0");
             model.addAttribute("waiverComment", "N/A");
         }
 
